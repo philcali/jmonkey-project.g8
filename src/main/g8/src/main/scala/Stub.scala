c1456e5d6d3a5a0300d72087cadbdea60df359e4
Philip Cali <philip.cali@gmail.com>
2011-04-21 00:04:21 -0500

    Copied jMonkey tutorial code for template.

--- src/main/g8/src/main/scala/Stub.scala
+++ src/main/g8/src/main/scala/Stub.scala
@@ -1,3 +1,21 @@
-class Stub {
-  // got any helpful boilerplate for your users?
+package $package$
+
+import com.jme3.app.SimpleApplication
+import com.jme3.material.Material
+import com.jme3.math.Vector3f
+import com.jme3.scene.Geometry
+import com.jme3.scene.shape.Box
+import com.jme3.math.ColorRGBA
+
+object $class$ extends SimpleApplication with Application {
+  this.start()
+
+  def simpleInitApp {
+    val b = new Box(Vector3f.ZERO, 1, 1, 1);
+    val geom = new Geometry("Box", b);
+    val mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+    mat.setColor("Color", ColorRGBA.Blue);
+    geom.setMaterial(mat);
+    rootNode.attachChild(geom);
+  }
 }
